3fa2826870af07c8b1e023778a18ca13ef01413e
Revateesa Dammalapati <revateesadammalapati@Revateesas-MBP.lan>
2025-04-22 14:18:58 -0500

    removing Game app

--- GameApp.java
+++
@@ -1,9 +0,0 @@
-public class GameApp {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            GameModel model = new GameModel(4); // 4x4 = 16 cards
-            GameView view = new GameView(model);
-            new GameController(model, view); // Controller & Timer Manager's job
-        });
-    }
-}
